08ddb27c7fdaae03db5afcc208306f43a802b5d0
sruthy-nr <sruthyn.@nestgroup.net>
2022-11-27 23:35:29 +0530

    edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentEntryComponent } from './student-entry/student-entry.component';
 import { StudentSearchComponent } from './student-search/student-search.component';
+import { StudentEditComponent } from './student-edit/student-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentEntryComponent,
-    StudentSearchComponent
+    StudentSearchComponent,
+    StudentEditComponent
   ],
   imports: [
     BrowserModule,
