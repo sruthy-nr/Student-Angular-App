e16f7d1100739913f35f7c48afcfd859b72820e8
sruthy-nr <sruthyn.@nestgroup.net>
2022-11-29 02:27:22 +0530

    readadd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { StudentDeleteComponent } from './student-delete/student-delete.componen
 import { StudentViewComponent } from './student-view/student-view.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
 
 const myRoute:Routes=[
   {
@@ -46,7 +47,8 @@ const myRoute:Routes=[
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(myRoute)
+    RouterModule.forRoot(myRoute),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/student-entry/student-entry.component.ts
+++ src/app/student-entry/student-entry.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./student-entry.component.css']
 })
 export class StudentEntryComponent {
-
+  name=""
+  adm=""
+  roll=""
+  mob=""
+  clg=""
+  pname=""
+  pphn=""
+  uname=""
+  pwd=""
+  add=()=>{
+    let data:any={
+      "name":this.name,"adm":this.adm,"roll":this.roll,"mob":this.mob,"clg":this.clg,"pname":this.pname,"pphn":this.pphn,"uname":this.uname,"pwd":this.pwd
+    }
+    console.log(data)
+  }
 }
